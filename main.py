933b2d07f7f435f6614f4f2a471f8b7b12b25874
Ashwin Ram Kumar <reachashrk@gmail.com>
2020-02-14 20:12:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from nlp2sql import NLP2sql
 
 def main():
     arg_parser = argparse.ArgumentParser(description='Natural Language to SQL query')
-    arg_parser.add_argument('-d', '--database', help='Path to SQL dump file', required=True)
-    arg_parser.add_argument('-l', '--language', help='Path to language configuration file', required=True)
+    arg_parser.add_argument('-d', '--database', help='Path to SQL dump file', default="~\school.sql")
+    arg_parser.add_argument('-l', '--language', help='Path to language configuration file', default="~\english.csv")
     arg_parser.add_argument('-i', '--sentence', help='Input sentence to parse', required=True)
     arg_parser.add_argument('-j', '--json_output', help='path to JSON output file', default=None)
     arg_parser.add_argument('-t', '--thesaurus', help='path to thesaurus file', default=None)
